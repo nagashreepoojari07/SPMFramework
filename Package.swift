cd82488826c4c3e9cead4e784220a20a922e113e
nagashreepoojari07 <56950098+nagashreepoojari07@users.noreply.github.com>
2022-12-07 12:21:38 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,11 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "SPMFramework",
+//     platforms: [
+//         .macOS(.v10_11),
+//         .iOS(.v9),
+//         .tvOS(.v9),
+//         .watchOS(.v2)
+//     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SPMFramework",
-            targets: ["SPMFramework"]),
+            name: "SPMFramework.framework",
+            targets: ["SPMFramework.framework"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
